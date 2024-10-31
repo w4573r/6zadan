7ba342b4bee6e998487e5cb2752639f9c3620e6b
dawid <dawidbratek3@gmail.com>
2024-10-31 22:43:03 +0100

    pisanie w konsoli

--- Data_Logger/dataLogger.js
+++ Data_Logger/dataLogger.js
@@ -1,2 +1,13 @@
 const fs = require('fs/promises');
 const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function askQuestion(question) {
+    return new Promise((resolve) => {
+        rl.question(question, (answer) => resolve(answer));
+    });
+}
