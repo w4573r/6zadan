84f7caf2b12be940db0deb0a3ed794bebe1a1834
dawid <dawidbratek3@gmail.com>
2024-11-01 17:37:37 +0100

    rejestrowanie dzialan

--- File_Watcher_Monitor_File_Changes/FWMFC.js
+++ File_Watcher_Monitor_File_Changes/FWMFC.js
@@ -1,3 +1,10 @@
 const fs = require('fs');
 
 const folderPath = './test';
+
+function logToFile(message) {
+    const logFilePath = './zmiany.txt';
+    fs.appendFile(logFilePath, message + '\n', (err) => {
+        if (err) throw err;
+    });
+}
