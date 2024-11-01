07748dd034b7a1677c437a7b43bf0d36289a9235
dawid <dawidbratek3@gmail.com>
2024-11-01 02:56:23 +0100

    odczytywanie

--- File_Analyzer/fileA.js
+++ File_Analyzer/fileA.js
@@ -3,3 +3,14 @@ const path = require('path');
 const EventEmitter = require('events');
 
 const events = new EventEmitter();
+
+function analyzeDirectory(directoryPath) {
+    console.log(`Analiza katalogu: ${directoryPath}`);
+
+    events.emit('analysisStarted');
+
+    fs.readdir(directoryPath, (err, items) => {
+        if (err) return console.error('Nie można odczytać katalogu');
+
+});
+}
